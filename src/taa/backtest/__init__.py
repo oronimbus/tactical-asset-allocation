ff7025a338c1c04b67d010983abd053cafc4d8f3
oronimbus <claudio.fontana@hotmail.ch>
2023-05-09 18:54:05 +0100

    run black

--- src/taa/backtest/__init__.py
+++ src/taa/backtest/__init__.py
@@ -1,8 +1,7 @@
 """Feature backtest modules."""
 from taa.backtest.performance import Tearsheet
 from taa.backtest.positions import EqualWeights, Positions, RiskParity
-from taa.backtest.returns import (Backtester, get_historical_price_data,
-                                  get_historical_total_return)
+from taa.backtest.returns import Backtester, get_historical_price_data, get_historical_total_return
 
 __all__ = [
     "Tearsheet",

9fee5c7d6a5d26923a9790a4922b7255821b1da6
oronimbus <claudio.fontana@hotmail.ch>
2023-07-06 19:22:24 +0100

    simplify logger

--- src/pytaa/tools/logger.py
+++ src/pytaa/tools/logger.py
@@ -4,9 +4,6 @@ import logging
 
 def setup_logger():
     """Basic configuration of logger for consistent format."""
-    for handler in logging.root.handlers[:]:
-        logging.root.removeHandler(handler)
-
     logging.basicConfig(
         format="%(asctime)s %(name)s %(levelname)s: %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",

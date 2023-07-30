cc86548f6a7e4bbd6e37a24f711a2da1309c9777
oronimbus <claudio.fontana@hotmail.ch>
2023-07-30 18:42:44 +0100

    finalize unit test for tearsheet

--- tests/backtest/test_performance.py
+++ tests/backtest/test_performance.py
@@ -9,7 +9,22 @@ TEST_RETURNS = pd.DataFrame(
     index=pd.date_range("2000-01-01","2000-01-12")
 )
 TEST_RETURNS.index.name = "Date"
-TEST_TS = None
+TEST_TS = {
+    "#obs": 12,
+    "#years": 0.030116358658453114,
+    "Total Return": -0.017995569871820605,
+    "Annual. Return": -0.45281952334930353,
+    "Volatility": 0.08359975532804562,
+    "MaxDD": -0.07850000000000013,
+    "Skewness": -0.8536696648152846,
+    "Kurtosis": 2.8851688725498037,
+    "Sharpe Ratio": -0.17823018669770466,
+    "StdErr": 1.0006236000757347,
+    "Sortino": -0.26339727599198887,
+    "Calmar": 5.768401571328698,
+    "HWM": 1.044867852278594,
+    "AutoCorr": -0.19978232211975622,
+}
 
 
 @pytest.mark.parametrize("returns, bm, expected", [
@@ -18,5 +33,5 @@ TEST_TS = None
 def test_tearsheet(returns, bm, expected):
     """Test tearsheet summary."""
     ts = Tearsheet(returns, bm).summary()
-    print(ts)
-    pass
+    for key in expected.keys():
+        assert np.isclose(ts.loc[key].values[0], expected[key])

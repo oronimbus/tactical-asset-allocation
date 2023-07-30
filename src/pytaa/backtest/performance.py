348644fbf96bb9a27873a0d75afa235308e3c7ca
oronimbus <claudio.fontana@hotmail.ch>
2023-07-30 19:34:37 +0100

    add benchmark statistics

--- src/pytaa/backtest/performance.py
+++ src/pytaa/backtest/performance.py
@@ -29,38 +29,49 @@ class Tearsheet:
             pd.DataFrame: table with post-trade statistics
         """
         n_obs = self.returns.shape[0]
-        t_years = (self.returns.index[-1] - self.returns.index[0]).days / 365.25
+        t_years = 1 / ((self.returns.index[-1] - self.returns.index[0]).days / 365.25)
         cum_return = self.returns.add(1).cumprod()
-        annual_return = cum_return.iloc[-1, :] ** (1 / t_years) - 1
+        annual_return = cum_return.iloc[-1, :] ** t_years - 1
         skew = self.returns.skew()
         kurt = self.returns.kurt() + 3
         volatility = self.returns.std()
-        sharpe = self.returns.mean() / volatility  * np.sqrt(ann_factor)
+        sharpe = self.returns.mean() / volatility * np.sqrt(ann_factor)
         # se_sr = np.sqrt((1 + np.square(sharpe) / 4 * (kurt - 1) - sharpe * skew) / t_years)
-        se_sr = np.sqrt(ann_factor / n_obs * (1 + (kurt - 1) / (4 * ann_factor) * sharpe ** 2))
+        se_sr = np.sqrt(ann_factor / n_obs * (1 + (kurt - 1) / (4 * ann_factor) * sharpe**2))
         maxdd = cum_return.div(cum_return.cummax()).sub(1).min()
         downside_vol = self.returns[self.returns < 0].std()
 
         summary = OrderedDict(
             {
                 "#obs": int(n_obs),
-                "#years": t_years,
+                "#years": 1 / t_years,
                 "Total Return": cum_return.iloc[-1, :] - 1,
-                "Annual. Return": annual_return,
+                "Annual Return": annual_return,
                 "Volatility": volatility * np.sqrt(ann_factor),
-                "MaxDD": maxdd,
+                "Max Drawdown": maxdd,
                 "Skewness": skew,
                 "Kurtosis": kurt,
                 "Sharpe Ratio": sharpe,
-                "StdErr": se_sr,
+                "Standard Error": se_sr,
                 "Sortino": self.returns.mean() / downside_vol * np.sqrt(ann_factor),
                 "Calmar": annual_return / maxdd,
                 "HWM": cum_return.max(),
-                "AutoCorr": autocorrelation(self.returns),
+                "Auto Correlation": autocorrelation(self.returns),
             }
         )
-        
+
         if self.benchmark is not None:
-            excess_return = self.returns.drop(self.benchmark, axis=1) - self.returns[self.benchmark]
-            summary["InfoRatio"] = np.mean(excess_return) / np.std(excess_return) * ann_factor
+            excess_return = self.returns.sub(self.returns[self.benchmark].values.reshape(-1, 1))
+            covar = self.returns.cov()[self.benchmark]
+            up_period = self.returns[self.returns.loc[:, self.benchmark] > 0].index
+            down_period = self.returns[self.returns.loc[:, self.benchmark] < 0].index
+            up_capture = self.returns.loc[up_period, :].add(1).cumprod().iloc[-1] ** t_years
+            down_capture = self.returns.loc[down_period, :].add(1).cumprod().iloc[-1] ** t_years
+
+            summary["Tracking Error"] = excess_return.std()
+            summary["Info Ratio"] = excess_return.mean() / excess_return.std()
+            summary["Beta"] = covar.div(self.returns[self.benchmark].var())
+            summary["Up Capture"] = up_capture / up_capture[self.benchmark] - 1
+            summary["Down Capture"] = down_capture / down_capture[self.benchmark] - 1
+            summary["Capture Ratio"] = summary["Up Capture"] / summary["Down Capture"]
         return pd.DataFrame(summary).T
